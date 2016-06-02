bb1400847cb65788ac2536775f0f4aa6d67f8746
Noora Al-Mana <nalmana.babson@gmail.com>
2016-06-01 20:21:20 -0400

    Added basic js to index page

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$("#enter").mouseenter(function() {
+		$(this).css("font-size", "28pt");
+	});
+	$("#enter").mouseleave(function() {
+		$(this).css("font-size", "24pt");
+	});
+
+});
